c48e9a05a15be33ebfc749f8c8ba90968509990c
zhangyue93 <yue.zhang@dowell.co.jp>
2022-08-10 13:40:47 +0900

    Create Thead.vue

---
+++ vuetify_todo/todo-vuetify/todolist/src/components/Thead.vue
@@ -0,0 +1,26 @@
+<template>
+		<v-banner 
+		padless
+		color="#006064"
+		single-line
+		rounded
+		>
+			<v-col
+			class="text-center font-weight-black text-h3 white--text"
+			cols="12"
+			>{{title}}
+			</v-col>
+		</v-banner>
+
+</template>
+
+<script>
+	export default{
+		name:"t-head",
+		data(){
+			return{
+				title:"ToDoリスト"
+			}
+		}
+	}
+</script>
