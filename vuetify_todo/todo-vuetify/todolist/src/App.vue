23196c60ccddf504b713f455e770d05091300db1
zhangyue93 <yue.zhang@dowell.co.jp>
2022-08-10 13:40:29 +0900

    Create App.vue

---
+++ vuetify_todo/todo-vuetify/todolist/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<v-app>
+  <div id="app">
+	<t-header></t-header>
+	<t-body class="space"></t-body>
+	<t-foot></t-foot>
+  </div>
+</v-app>
+</template>
+
+<script>
+import Tbody from './components/Tbody.vue';
+import Thead from './components/Thead.vue';
+import Tfoot from './components/Tfoot.vue';
+
+export default {
+	name: 'App',
+	components: {
+		"t-header":Thead,
+		"t-body":Tbody,
+		"t-foot":Tfoot
+  },
+
+  data: () => ({
+    
+  }),
+};
+</script>
+
+<style>
+	.space{
+		margin-top: 80px;
+		margin-bottom: 80px;
+		margin-left: 30px;
+	}
+</style>
