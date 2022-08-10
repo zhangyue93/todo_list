31987cfb2665e5167a7570a6e7346267468f5c51
zhangyue93 <yue.zhang@dowell.co.jp>
2022-08-10 13:40:44 +0900

    Create Tfoot.vue

---
+++ vuetify_todo/todo-vuetify/todolist/src/components/Tfoot.vue
@@ -0,0 +1,24 @@
+<template>
+		<v-footer padless>
+		    <v-col
+		      class="text-center"
+		      cols="12"
+		    >
+		      {{copyright}}
+		    </v-col>
+		  </v-footer>
+</template>
+
+<script>
+	export default{
+		name:"t-foot",
+		data(){
+			return{
+				copyright:"Vuetify demo 2020-08-09"
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
